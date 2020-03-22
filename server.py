86c19236eaeecbc12fbf151ae30d022b2636d167
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-03-22 13:01:15 +0900

    Add forget )

--- server.py
+++ server.py
@@ -2,5 +2,6 @@ from main import app
 import os
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000)
+    
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
